b7b561e52309e5f33ce856f2f63c824ca3bc1077
MeherKeertanä <meherkeertana19@gmail.com>
2022-06-15 17:34:23 +0530

    Created Signup

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/abc')
 def index():
   return render_template('index.html')
+
+@app.route('/abc/signup')
+def signup():
+  return render_template('signup_modal.html')
+
+@app.route('/signup_modal.html')
+def signup_index():
+  return render_template('signup_modal.html')
+
+
+
