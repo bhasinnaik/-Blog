8b470d6599687f1ee3dc92429bfd97d236609a8a
Bhasin Naik <Bhasin@Bhasins-MacBook-Air.local>
2022-06-19 15:51:01 +0530

    added comment in app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
-
+# Define database URL String
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# DB Object
 db = SQLAlchemy(app)
 
+# DB Model
+# To create db, import object in python shell and than execute create comment with db object.
 class Blog(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   version = db.Column(db.String(3))
@@ -15,6 +18,7 @@ class Blog(db.Model):
   date_posted = db.Column(db.DateTime)
   content = db.Column(db.Text)
 
+#Root Index path.
 @app.route('/')
 def index():
   posts = Blog.query.all()
